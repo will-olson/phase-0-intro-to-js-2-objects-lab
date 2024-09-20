010c5c5a4e6356fa921ace3b8d7adfba3c36000c
Will Olson <will.olson.b@gmail.com>
2024-09-20 13:16:21 -0500

    Assign each employee to an object and created four functions to resolve errors

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+let employee = {
+  name: "John Doe",
+  address: "123 Main St"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+module.exports = {
+  employee,
+  updateEmployeeWithKeyAndValue,
+  destructivelyUpdateEmployeeWithKeyAndValue,
+  deleteFromEmployeeByKey,
+  destructivelyDeleteFromEmployeeByKey
+};
